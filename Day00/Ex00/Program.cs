020e0e089947b16de52eadc255b40bb400d7e2ca
Zera57 <svetik.avtonomov@gmail.com>
2021-05-28 11:36:55 +0300

    Maded Day00

---
+++ Day00/Ex00/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+double sum;							// Сумма кредита, р
+double totalDebt;					// Сумма общего долга - сумма основного долга на дату расчета.
+double rate;						// Годовая процентная ставка, %
+double rateMonth;					// i — процентная ставка по займу в месяц.
+int term;							// Количество месяцев кредита
+int selectedMonth;					// Номер месяца кредита, в котором вносится досрочный платёж
+double payment;						// Сумма досрочного платежа, р
+double paymentMonth;				// Аннуитетный платеж
+double paymentPercentage;			// Проценты ежемесячного платежа
+double totalPercentage = 0;           // Переплата
+bool parsing = true;				// Флаг введенных данных
+if (args.Length != 5)
+{
+	Console.WriteLine("Error you must have exactly 5 args!");
+	return;
+}
+parsing &= double.TryParse(args[0], out sum);
+parsing &= double.TryParse(args[1], out rate);
+parsing &= int.TryParse(args[2], out term);
+parsing &= int.TryParse(args[3], out selectedMonth);
+parsing &= double.TryParse(args[4], out payment);
+if (!parsing)
+{
+	Console.WriteLine("Error you must have valid args!");
+	return;
+}
+totalDebt = sum;
+rateMonth = rate / 12 / 100;
+while(totalDebt > 0)
+{
+	paymentMonth = (sum * rateMonth * Math.Pow(1 + rateMonth, term))
+						/ (Math.Pow(1 + rateMonth, term) - 1);
+
+	paymentPercentage = (totalDebt * rateMonth * DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.AddMonths(1).Month))
+							/ (100 * (DateTime.IsLeapYear(DateTime.Now.Year) ? 366 : 365));
+	totalPercentage += paymentMonth / 100 * paymentPercentage;
+	totalDebt -= paymentMonth - (paymentMonth / 100 * paymentPercentage);
+	Console.Write(" Payment = " + Math.Round(paymentMonth));
+	Console.Write("\t\tclosing sum = " + (paymentMonth - (paymentMonth / 100 * paymentPercentage)));
+	Console.WriteLine("\t\tPercentage = " + Math.Round(paymentMonth / 100 * paymentPercentage));
+	Console.WriteLine("Total debt " + totalDebt);
+}
+Console.WriteLine("Total pereplata " + totalPercentage);
---
+++ Day00/Ex01/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+if (!System.IO.File.Exists("names.txt"))
+{
+	Console.WriteLine("Cannot find names.txt");
+	return;
+}
+string[] namesDB = System.IO.File.ReadAllLines("names.txt");
+int[] distanceDB = new int[namesDB.Length];
+
+Console.WriteLine("Enter name:");
+string inputS = Console.ReadLine().Trim();
+if (!validateName(inputS))
+{
+	Console.WriteLine("Not valid name");
+	return;
+}
+
+for (int i = 0; i < namesDB.Length; i++)
+{
+	distanceDB[i] = levensteinDistance(inputS, namesDB[i]);
+	if (distanceDB[i] == 0)
+	{
+		Console.WriteLine($"Hello, {inputS}!");
+		return;
+	}
+}
+int minDistance = 3;
+for (int i = 0; i < distanceDB.Length; i++)
+{
+	if (minDistance > distanceDB[i])
+	{
+		minDistance = distanceDB[i];
+	}
+}
+for (int i = 0; i < distanceDB.Length; i++)
+{
+	if (distanceDB[i] == minDistance)
+	{
+		Console.WriteLine($"Did you mean {namesDB[i]}?");
+		if (Console.ReadLine().CompareTo("Y") == 0)
+		{
+			Console.WriteLine($"Hello, {namesDB[i]}!");
+			return;
+		}
+	}
+}
+Console.WriteLine("Your name was not found.");
+
+bool validateName(string name)
+{
+	if (name.Length == 0)
+		return (false);
+	return Regex.Match(name, "^[A-Za-z]*-?[A-Za-z]*$").Success;
+}
+
+int levensteinDistance(string a, string b)
+{
+	int[,] f = new int[a.Length + 1, b.Length + 1];
+
+	for (int i = 0; i < a.Length + 1; i++)
+	{
+		for (int j = 0; j < b.Length + 1; j++)
+		{
+			if (i * j == 0)
+				f[i, j] = i + j;
+		}
+	}
+	for (int i = 1; i < a.Length + 1; i++)
+	{
+		for (int j = 1; j < b.Length + 1; j++)
+		{
+			if (a[i - 1] == b[j - 1])
+				f[i, j] = f[i - 1, j - 1];
+			else
+				f[i, j] = 1 + Math.Min(Math.Min(f[i - 1, j], f[i, j - 1]), f[i - 1, j - 1]);
+		}
+	}
+	return (f[a.Length, b.Length]);
+}
